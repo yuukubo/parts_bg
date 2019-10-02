66cae1b386e3f338894cf8979569eb96c6ad1654
yuukubo <i.yusuke.shk@gmail.com>
2019-10-03 00:22:58 +0900

    background loop test.

--- sketch.js
+++ sketch.js
@@ -4,20 +4,33 @@ let [canvasx, canvasy] = [720, 690];
 let [canvasFromx, canvasFromy] = [0, 0];
 let blockw = canvasx;
 let blockh = 10;
-let blockspdy = 1;
+let blockspdy = 3;
 let blockspdx = blockw;
-let blockx = 0;
-let blocky = 0;
+let blockx1 = 0;
+let blocky1 = 0;
+let blockx2 = 0;
+let blocky2 = -1 * canvasy;
+let blocknum = 0;
 
 function setup() {
   createCanvas(canvasx, canvasy);
+  blocknum = canvasy / blockh;
 }
 
 function draw() {
   background(0);
-  fill(100);
-  rect(blockx, blocky, blockw, blockh);
-  blocky += blockspdy;
+  noStroke();
+  for (let i = 0; i < blocknum; i++) {
+    fill(i * 2);
+    rect(blockx1, blocky1 + i * blockh, blockw, blockh);  
+  }
+  blocky1 += blockspdy;
+  if (canvasy <= blocky1 + blockh) { blocky1 = canvasFromy - canvasy }
 
-  if (canvasy <= blocky + blockh) { blocky = canvasFromy }
+  for (let i = 0; i < blocknum; i++) {
+    fill(blocknum * 2 - i * 2);
+    rect(blockx2, blocky2 + i * blockh, blockw, blockh);  
+  }
+  blocky2 += blockspdy;
+  if (canvasy <= blocky2 + blockh) { blocky2 = canvasFromy - canvasy }
 }
