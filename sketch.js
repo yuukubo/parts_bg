899aea3d7f1798a5aca0bd0a761d4b6872a2be4e
yuukubo <i.yusuke.shk@gmail.com>
2019-10-04 21:52:13 +0900

    grid test.

--- sketch.js
+++ sketch.js
@@ -50,7 +50,10 @@ function draw() {
   push();
   stroke(255);
   for (let i = 0; i < lineynum; i++) {
-    line(linexfr, blocky1 + i * lineyinterval, linexto, lineyfr + i * lineyinterval);  
+    line(linexfr, lineyfr + i * lineyinterval, linexto, lineyfr + i * lineyinterval);  
+  }
+  for (let i = 0; i < linexnum; i++) {
+    line(linexfr + i * linexinterval, lineyfr, linexfr + i * linexinterval, lineyto);  
   }
   pop();
 }
