c9b305b2fe8d6312c6713e56d21cbffd92ae84e6
yuukubo <i.yusuke.shk@gmail.com>
2019-10-07 23:26:04 +0900

    mode test

--- sketch.js
+++ sketch.js
@@ -22,6 +22,9 @@ let lineynum = 0;
 let age = 0;
 let isswing = 0;
 let swingtimer = 0;
+let ismode1 = 0;
+let ismode2 = 0;
+let ismode3 = 0;
 
 function setup() {
   createCanvas(canvasx, canvasy);
@@ -34,46 +37,61 @@ function draw() {
   background(0);
   age++;
 
-  push();
-  noStroke();
-  for (let i = 0; i < blocknum; i++) {
-    fill(i * 2);
-    rect(blockx1, blocky1 + i * blockh, blockw, blockh);
+  if (keyIsDown(49)) {
+    ismode1 = 1;
   }
-  blocky1 += blockspdy;
-  if (canvasy <= blocky1 + blockh) { blocky1 = canvasFromy - canvasy }
-
-  for (let i = 0; i < blocknum; i++) {
-    fill(blocknum * 2 - i * 2);
-    rect(blockx2, blocky2 + i * blockh, blockw, blockh);
+  if (keyIsDown(50)) {
+    ismode2 = 1;
   }
-  blocky2 += blockspdy;
-  if (canvasy <= blocky2 + blockh) { blocky2 = canvasFromy - canvasy }
-  pop();
-
-  if (age % 600 === 0) {
-    isswing = 1;
-  }
-  if (isswing) {
-    swingtimer++;
-    if (0 < swingtimer && swingtimer <= 20) {
-      linexfr++;
-      linexto++;
-    }
-    if (20 < swingtimer && swingtimer <= 40) {
-      linexfr -= 2;
-      linexto -= 2;
-    }
-    if (40 < swingtimer && swingtimer <= 60) {
-      linexfr++;
-      linexto++;
-    }
-    if (60 < swingtimer) {
-      swingtimer = 0;
-      isswing = 0;
-    }
+  if (keyIsDown(51)) {
+    ismode3 = 1;
   }
 
+  if (ismode1) {
+    push();
+    noStroke();
+    for (let i = 0; i < blocknum; i++) {
+      fill(i * 2);
+      rect(blockx1, blocky1 + i * blockh, blockw, blockh);
+    }
+    blocky1 += blockspdy;
+    if (canvasy <= blocky1 + blockh) { blocky1 = canvasFromy - canvasy }
+
+    for (let i = 0; i < blocknum; i++) {
+      fill(blocknum * 2 - i * 2);
+      rect(blockx2, blocky2 + i * blockh, blockw, blockh);
+    }
+    blocky2 += blockspdy;
+    if (canvasy <= blocky2 + blockh) { blocky2 = canvasFromy - canvasy }
+    pop();
+  }
+
+  if (ismode2) {
+    if (age % 600 === 0) {
+      isswing = 1;
+    }
+    if (isswing) {
+      swingtimer++;
+      if (0 < swingtimer && swingtimer <= 20) {
+        linexfr++;
+        linexto++;
+      }
+      if (20 < swingtimer && swingtimer <= 40) {
+        linexfr -= 2;
+        linexto -= 2;
+      }
+      if (40 < swingtimer && swingtimer <= 60) {
+        linexfr++;
+        linexto++;
+      }
+      if (60 < swingtimer) {
+        swingtimer = 0;
+        isswing = 0;
+      }
+    }
+  }
+
+  if (ismode3) {
     push();
     stroke(255, 100);
     for (let i = 0; i < lineynum; i++) {
@@ -84,3 +102,4 @@ function draw() {
     }
     pop();
   }
+}
